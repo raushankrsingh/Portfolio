471304dc43e4a2e9c21023183acfb4c09bf2716f
Raushan <singhraushan9923@gmail.com>
2023-04-27 00:18:26 +0530

    minor changes

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            developer.
+            develop.
           </a>
           <div className="itemContainer">
             <Person className="icon" />
